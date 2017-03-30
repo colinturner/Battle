d0fa720da0689836adbef0a7863dc9577e22455d
Colin Turner <colinturner34@gmail.com>
2017-03-30 18:56:41 +0100

    Refactor app.rb - removed comments

--- app.rb
+++ app.rb
@@ -16,23 +16,18 @@ class Battle < Sinatra::Base
   post "/names" do
     p params
     $game = Game.new(Player.new(params["Player 1"]), Player.new(params["Player 2"]))
-    # $player1 = Player.new(params["Player 1"])
-    # $player2 = Player.new(params["Player 2"])
     redirect '/play'
   end
 
   get '/play' do
     $game.switch_player
-    # $p1_health = 100
-    # $p2_health = 100
+
     erb(:play)
   end
 
   get '/attack' do
     $game.attack($game.injured_player)
     erb :attack
-
-    #$game.turn += 1
   end
 
   run! if app_file == $0
