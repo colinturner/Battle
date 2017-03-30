ed22f50c57481da024e71236825026ec7bf54b95
Colin Turner <colinturner34@gmail.com>
2017-03-30 10:27:02 +0100

    Extracted logic to the model layer. Introduced global variables

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'shotgun'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -13,22 +14,18 @@ class Battle < Sinatra::Base
 
   post "/names" do
     p params
-    session[:player1] = params["Player 1"]
-    session[:player2] = params["Player 2"]
+    $player1 = Player.new(params["Player 1"])
+    $player2 = Player.new(params["Player 2"])
     redirect '/play'
   end
 
   get '/play' do
     $p1_health = 100
     $p2_health = 100
-    @player1 = session[:player1]
-    @player2 = session[:player2]
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
     erb :attack
   end
 
--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -2,6 +2,6 @@ feature 'attack player' do
   scenario 'attack a player' do
     sign_in_and_play
     click_link "Cheat emissions test!"
-    expect(page).to have_content "Gas everywhere. Polar Bear -12HP"
+    expect(page).to have_content "Volkswagen used Cheat Emissions. It's super effective! Polar Bear -12 HP. Polar Bear HP: 88"
   end
 end
